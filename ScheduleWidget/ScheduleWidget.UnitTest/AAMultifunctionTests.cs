90b68652324c7d5769ea123fb7e39b4e8b5cf78b
James Still <james.still@gmail.com>
2014-09-26 10:05:06 -0700

    pull request 25

---
+++ ScheduleWidget/ScheduleWidget.UnitTest/AAMultifunctionTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using ScheduleWidget.ScheduledEvents;
+using ScheduleWidget.Enums;
+using ScheduleWidget.TemporalExpressions;
+using System.Collections.Generic;
+
+namespace ScheduleWidget.UnitTest
+{
+    [TestFixture]
+    public class AAMultifunctionTests
+    {
+        [Test]
+        public void AAMultifunctionTest1()
+        {
+            var aEvent = new Event()
+            {
+                FrequencyTypeOptions = FrequencyTypeEnum.Yearly,
+                RepeatInterval = 2,
+                StartDateTime = new DateTime(2000, 9, 27), // even years only
+                Anniversary = new Anniversary()
+                {
+                    Month = 9,
+                    Day = 27
+                }
+            };
+            // Occurs 2000,2002,2004,2006,2008,2010
+            aEvent.SetEndDateWithNumberOfOccurrences(6);
+
+            // Check that the ending date was set correctly.
+            Assert.IsTrue(aEvent.EndDateTime == new DateTime(2010, 9, 27));
+
+            // Check that the number of occurrences is retrievable.
+            Assert.IsTrue(aEvent.NumberOfOccurrencesThatWasLastSet == 6);
+
+            // Exclude 2000,2006,2010.
+            List<DateTime> excludedDates = new List<DateTime>();
+            excludedDates.Add(new DateTime(2000, 9, 27));
+            excludedDates.Add(new DateTime(2005, 9, 27));
+            excludedDates.Add(new DateTime(2005, 9, 28));
+            excludedDates.Add(new DateTime(2006, 9, 27));
+            excludedDates.Add(new DateTime(2010, 9, 27));
+            var schedule = new Schedule(aEvent, excludedDates);
+
+            // Make sure it is not occurring on excluded dates.
+            Assert.IsFalse(schedule.IsOccurring(new DateTime(2000, 9, 27)));
+            Assert.IsFalse(schedule.IsOccurring(new DateTime(2006, 9, 27)));
+            Assert.IsFalse(schedule.IsOccurring(new DateTime(2010, 9, 27)));
+
+            // Make sure it is not occurring outside the set range.
+            Assert.IsFalse(schedule.IsOccurring(new DateTime(1998, 9, 27)));
+            Assert.IsFalse(schedule.IsOccurring(new DateTime(2012, 9, 27)));
+
+            // Make sure it is occurring on desired dates.
+            Assert.IsTrue(schedule.IsOccurring(new DateTime(2002, 9, 27)));
+            Assert.IsTrue(schedule.IsOccurring(new DateTime(2004, 9, 27)));
+            Assert.IsTrue(schedule.IsOccurring(new DateTime(2008, 9, 27)));
+
+            // Check the occurrences function.
+            DateRange during = new DateRange(new DateTime(1995, 1, 1),new DateTime(2015, 1, 1));
+            var occurrences = schedule.Occurrences(during);
+            Assert.IsTrue(occurrences.Count() == 3);
+
+            // Check the last occurrence date function.
+            DateTime? lastDate = schedule.GetLastOccurrenceDate();
+            Assert.IsTrue(lastDate == new DateTime(2008, 9, 27));
+
+            // Check the next occurrence (date only) function.
+            Assert.IsTrue(schedule.NextOccurrence(new DateTime(1995, 1, 1)) == new DateTime(2002, 9, 27));
+            Assert.IsTrue(schedule.NextOccurrence(new DateTime(2004, 9, 26)) == new DateTime(2004, 9, 27));
+            Assert.IsTrue(schedule.NextOccurrence(new DateTime(2004, 9, 27)) == new DateTime(2008, 9, 27));
+            Assert.IsTrue(schedule.NextOccurrence(new DateTime(2008, 9, 27)) == null);
+
+            // Check the previous occurrence (date only) function.
+            Assert.IsTrue(schedule.PreviousOccurrence(new DateTime(1995, 1, 1)) == null);
+            Assert.IsTrue(schedule.PreviousOccurrence(new DateTime(2004, 9, 26)) == new DateTime(2002, 9, 27));
+            Assert.IsTrue(schedule.PreviousOccurrence(new DateTime(2004, 9, 28)) == new DateTime(2004, 9, 27));
+            Assert.IsTrue(schedule.PreviousOccurrence(new DateTime(2013, 9, 27)) == new DateTime(2008, 9, 27));
+
+            // Check the next occurrence ranged function.
+            DateRange range1 = new DateRange(new DateTime(2004, 9, 1), new DateTime(2004, 10, 1));
+            DateRange range2 = new DateRange(new DateTime(2004, 11, 1), new DateTime(2002, 9, 27));
+            Assert.IsTrue(schedule.NextOccurrence(new DateTime(1995, 1, 1), range1) == new DateTime(2004, 9, 27));
+            Assert.IsTrue(schedule.NextOccurrence(new DateTime(1995, 1, 1), range2) == null);
+
+            // Check the previous occurrence ranged function.
+            Assert.IsTrue(schedule.PreviousOccurrence(new DateTime(2015, 1, 1), range1) == new DateTime(2004, 9, 27));
+            Assert.IsTrue(schedule.PreviousOccurrence(new DateTime(2015, 1, 1), range2) == null);
+        }
+    }
+}
---
+++ ScheduleWidget/ScheduleWidget/ScheduledEvents/Utilities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScheduleWidget.ScheduledEvents
+{
+    static public class Utilities
+    {
+
+        /// <summary>
+        /// DateTime.SafeAddDays,
+        /// This is an extension method.
+        /// Adds days to a date time object without the possibility of generating an out of range exception.
+        /// This is useful when the DateTime object might be set to minimum or maximum value.
+        /// </summary>
+        /// <returns>The new date time object.</returns>
+        static public DateTime SafeAddDays(this DateTime initialValue, double days)
+        {
+            try { return initialValue.AddDays(days); }
+            catch (Exception) { return (days > 0) ? DateTime.MaxValue: DateTime.MinValue; }
+        }
+        /// <summary>
+        /// DateTime.SafeAddYears,
+        /// This is an extension method.
+        /// Adds years to a date time object without the possibility of generating an out of range exception.
+        /// This is useful when the DateTime object might be set to minimum or maximum value.
+        /// </summary>
+        /// <returns>The new date time object.</returns>
+        static public DateTime SafeAddYears(this DateTime initialValue, int years)
+        {
+            try { return initialValue.AddYears(years); }
+            catch (Exception) { return (years > 0) ? DateTime.MaxValue : DateTime.MinValue; ; }
+        }
+    }
+}
