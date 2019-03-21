7cc0cd64ac217de51cf7ddcab97faf2218be9153
Anwar <anwar@skybin.io>
2019-03-21 23:11:11 +0530

    Upgrade Packages & Multi-Target .Net 4.5, 4.6, 4.7 & .Net Standard

--- ScheduleWidget/ScheduleWidget/Properties/AssemblyInfo.cs
+++ ScheduleWidget/ScheduleWidget/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ScheduleWidget")]
-[assembly: AssemblyDescription("Recurring Events for Calendars")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScheduleWidget")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,17 +9,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5b0fae42-ecee-4f73-a4ce-356943049282")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
 
 [assembly: InternalsVisibleTo("ScheduleWidget.UnitTest")]
